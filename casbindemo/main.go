7756d18f6c2da65d22baacd4d2b7f17669abcc03
Tomtao626 <751825253@qq.com>
2022-12-30 11:02:42 +0800

    fix casbin

--- casbindemo/main.go
+++ casbindemo/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/casbin/casbin/v2"
 	"log"
+	"strings"
 )
 
 func check(e *casbin.Enforcer, sub, obj, act string) {
@@ -15,11 +16,29 @@ func check(e *casbin.Enforcer, sub, obj, act string) {
 	}
 }
 
+func KeyMatcher(key1, key2 string) bool {
+	i := strings.Index(key2, "*")
+	if i == -1 {
+		return key1 == key2
+	}
+	if len(key1) > 1 {
+		return key1[:i] == key2[:i]
+	}
+	return key1 == key2[:i]
+}
+
+func KeyMatcherFunc(args ...interface{}) (interface{}, error) {
+	name1 := args[0].(string)
+	name2 := args[1].(string)
+	return bool(KeyMatcher(name1, name2)), nil
+}
+
 func main() {
 	e, err := casbin.NewEnforcer("./model.conf", "./policy.csv")
 	if err != nil {
 		log.Fatalf("NewEnforecer failed:%v\n", err)
 	}
+	e.AddFunction("my_func", KeyMatcherFunc)
 
 	check(e, "tomtao626", "data1", "read")
 	check(e, "tomtao627", "data2", "write")
